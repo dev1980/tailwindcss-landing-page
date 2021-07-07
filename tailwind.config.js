44ceba5964f81b7b7515f4da6a79310335bc2587
Brham Dev Mahato <brham1980@gmail.com>
2021-07-07 23:06:30 +0545

    fix the container break-point

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: ["Poppins', sans-serif;"],
     container:{center: true, 
     padding: "1rem",
+    screens: {
+      lg: "1124px",
+      xl: "1124px",
+      "2xl": "1124px",
+    }
   },
   },
   variants: {
